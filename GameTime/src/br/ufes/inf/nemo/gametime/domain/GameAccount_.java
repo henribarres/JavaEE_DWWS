b148337d218cb05218fc9a4a5dfefcbb85f3dc8a
castro <castro@castro-Inspiron-1564>
2015-05-04 10:34:55 -0300

    Messages refeito

--- GameTime/src/br/ufes/inf/nemo/gametime/domain/GameAccount_.java
+++ GameTime/src/br/ufes/inf/nemo/gametime/domain/GameAccount_.java
@@ -5,12 +5,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2015-05-01T14:38:01.422-0300")
+@Generated(value="Dali", date="2015-05-04T10:28:41.221-0300")
 @StaticMetamodel(GameAccount.class)
 public class GameAccount_ extends PersistentObjectSupport_ {
 	public static volatile SingularAttribute<GameAccount, String> name;
 	public static volatile SingularAttribute<GameAccount, String> login;
-	public static volatile SingularAttribute<GameAccount, String> password;
 	public static volatile SingularAttribute<GameAccount, GroupGame> groupGame;
+	public static volatile SingularAttribute<GameAccount, String> password;
 	public static volatile SingularAttribute<GameAccount, User> userOwner;
 }
