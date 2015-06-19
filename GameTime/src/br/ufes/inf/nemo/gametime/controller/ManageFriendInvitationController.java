d86af0801b2a5d3b58e15f8382ceb7cfce57ea4b
castro <castro@castro-Inspiron-1564>
2015-06-19 11:14:31 -0300

    Está enviando emails. Unico problema é se o email não existir ele envia mesmo assim. Mas funciona. Se quiser olhar mais a fundo fica a vontade, por mim está pronto!

--- GameTime/src/br/ufes/inf/nemo/gametime/controller/ManageFriendInvitationController.java
+++ GameTime/src/br/ufes/inf/nemo/gametime/controller/ManageFriendInvitationController.java
@@ -8,6 +8,16 @@ import br.ufes.inf.nemo.gametime.application.ManageFriendInvitationService;
 import br.ufes.inf.nemo.gametime.domain.Invitation;
 import br.ufes.inf.nemo.util.ejb3.controller.JSFController;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 @Named
 @SessionScoped
 public class ManageFriendInvitationController extends JSFController {
@@ -32,13 +42,49 @@ public class ManageFriendInvitationController extends JSFController {
 
 	public String register(){
 		InviteService.register(invite);
-		return  "/manageFriendInvitation/success.xhtml?faces-redirect=true" ;
+		boolean reply = sendEmail(invite.getEmail());
+		if (reply) {
+			return  "/manageFriendInvitation/success.xhtml?faces-redirect=true" ;
+		}
+		else{
+			return  "/manageFriendInvitation/fail.xhtml?faces-redirect=true" ;
+		}
 	}
 	
 	public String begin(){
 		invite = new Invitation();
 		return "/manageFriendInvitation/invite.xhtml?faces-redirect=true";
 	}
-	
-	
+
+	public boolean sendEmail(String email) {
+		final String username = "gametimebrapp@gmail.com";
+	    final String password = "wow123$%";
+
+	    Properties props = new Properties();
+	    props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() { 
+        	protected PasswordAuthentication getPasswordAuthentication() {
+        		return new PasswordAuthentication(username, password);
+            	}
+          	});
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("gametimebrapp@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject("Bem Vindo ao GameTime");
+            message.setText("Este e-mail foi enviado pelos seus amigos através do nosso sistema." + "\n\n Junte-se a nós para ter a melhor experiencia em jogos online, compartilhando contas, sem se preocupar, e fazendo novas amizades!" + "\n\n Acesse o site localhost:8080/GameTime e comece a jogar agora mesmo!" );
+
+            Transport.send(message);
+            return true;
+
+        } catch (MessagingException e) {
+            return false;
+        }
+	}
 }
